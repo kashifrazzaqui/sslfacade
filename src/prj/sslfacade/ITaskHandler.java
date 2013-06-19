9a69af3d9e7ca45fd593d7ce11c0e3761fd3e5ff
Kashif Razzaqui <kashif.razzaqui@gmail.com>
2013-06-20 01:23:45 +0530

    modify comment

--- src/prj/sslfacade/ITaskHandler.java
+++ src/prj/sslfacade/ITaskHandler.java
@@ -6,7 +6,7 @@ public interface ITaskHandler
 {
     /*
     In order to continue handshakes after tasks are processed the
-    Handshaker.carryOn method must be called.
+    tasks.done() method must be called.
      */
     public void process(Tasks tasks) throws IOException;
 }
